18a178efc7f04c0623ccfbe6cc46c4358a0289c5
Takacs-Balazs <71711153+Takacs-Balazs@users.noreply.github.com>
2023-05-24 22:58:56 +0200

    Add files via upload

---
+++ kesz/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace hazii;
+class Program
+{
+
+
+        static void Main(string[] args)
+        {
+            List<osztaly> Lista = new List<osztaly>();
+            using (StreamReader fajl = new StreamReader("kifejezesek.txt"))
+            {
+                while (!fajl.EndOfStream)
+                {Lista.Add(new osztaly(fajl.ReadLine()));}
+            }
+            Console.WriteLine($"kifejezesek: {Lista.Count}");
+            Console.WriteLine($"maradekos osztassal: {Lista.Count(x => x.STRING == "mod")}");
+            do
+            {
+                if (inp != "")
+                {try
+                    {osztaly muvelet = new osztaly(inp);
+                     Console.WriteLine($"\t{muvelet.ToString()}");}
+                    catch (Exception)
+                    {Console.WriteLine("\tHibás muvelet!");}}
+              Console.Write("Adj meg egy muveletet (pl: 9 - 27): ");
+              inp = Console.ReadLine();
+            } while (inp != "");
+            using (StreamWriter end = new StreamWriter("eredmenyek.txt"))
+            {Lista.ForEach(x => end.WriteLine(x.ToString()));}
+            Console.WriteLine("eredmenyek.txt");
+            Console.ReadKey();
+        }
+    }
---
+++ kesz/osztaly.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazii
+{
+    
+    internal class osztaly
+    {
+        int egyik, masik;
+        string strng;
+
+        public int Egyik { get => egyik; set => egyik = value; }
+        public int Masik { get => masik; set => masik = value; }
+        public string STRING { get => strng; set => strng = value; }
+
+        public string eredmeny
+        {
+           
+            {
+                try
+                {
+                    switch (STRING)
+                    {
+                        
+                    }
+                }
+                catch (Exception)
+                {
+                    return "hiba!";
+                }
+            }
+        }
+
+        public osztaly(string line)
+        {
+            string[] data = line.Split(',');
+            Egyik = int.Parse(data[0].Trim());
+            strng = data[1].Trim();
+            Masik = int.Parse(data[2].Trim());
+        }
+
+        public override string ToString()
+        {
+            return $"{Egyik} {STRING} {Masik} = {eredmeny}";
+        }
+    }
+}
+
+
